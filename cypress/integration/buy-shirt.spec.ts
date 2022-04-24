28f504abb1efa8404a69779c8ce0154522c716f0
DanielDi <despinalm@unal.edu.co>
2022-04-24 17:22:06 -0500

    step 8

--- cypress/integration/buy-shirt.spec.ts
+++ cypress/integration/buy-shirt.spec.ts
@@ -1,11 +1,17 @@
-describe("Buy a t-shirt", () => {
-  it("then the t-shirt should be bought", () => {
-    cy.visit("http://automationpractice.com/");
-    cy.get("#block_top_menu > ul > li:nth-child(3) > a").click();
-    cy.get("#center_column a.button.ajax_add_to_cart_button.btn.btn-default").click();
-    cy.get("[style*=\"display: block;\"] .button-container > a").click();
-    cy.get(".cart_navigation span").click();
+import { MenuContentPage } from '../page/index'
 
+const menuContentPage = new MenuContentPage()
+
+describe('Buy a t-shirt', () => {
+
+  it('then should be bought a t-shirt', () => {
+    menuContentPage.visitMenuContentPage()
+    menuContentPage.goToTShirtMenu()
+    cy.get("#center_column a.button.ajax_add_to_cart_button.btn.btn-default").click();
+    cy.get('[style*="display: block;"] .button-container > a').click()
+    cy.get('.cart_navigation span').click()
+
+    // El resto del flujo de la prueba....
     cy.get("#email").type("aperdomobo@gmail.com");
     cy.get("#passwd").type("WorkshopProtractor");
     cy.get("#SubmitLogin").click();
@@ -14,9 +20,9 @@ describe("Buy a t-shirt", () => {
     cy.get("#form > p > button").click();
     cy.get("#HOOK_PAYMENT > div:nth-child(1) > div > p > a").click();
     cy.get("#cart_navigation > button").click();
-    // Debes completar la prueba ... implementa los pasos 8 al 13, del proceso de compra
 
     cy.get("#center_column > div > p > strong")
       .should("have.text", "Your order on My Store is complete.");
+
   });
 });
